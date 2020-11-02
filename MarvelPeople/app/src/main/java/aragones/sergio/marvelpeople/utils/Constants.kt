28b975aef22de46379b6d1d11fbd93c096ad89b7
Sergio Aragonés <sergioat7@gmail.com>
2020-11-02 21:27:48 +0100

    Generated md5 hash automatically

--- MarvelPeople/app/src/main/java/aragones/sergio/marvelpeople/utils/Constants.kt
+++ MarvelPeople/app/src/main/java/aragones/sergio/marvelpeople/utils/Constants.kt
@@ -8,6 +8,8 @@ package aragones.sergio.marvelpeople.utils
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Scheduler
 import io.reactivex.rxjava3.schedulers.Schedulers
+import java.security.MessageDigest
+import kotlin.text.Charsets.UTF_8
 
 class Constants {
     companion object {
@@ -22,15 +24,22 @@ class Constants {
         const val OFFSET_PARAM = "offset"
         const val NAME_STARTS_WITH_PARAM = "nameStartsWith"
         const val LIMIT = 20
+        private const val PRIVATE_KEY = "659fd58aced58e8705daca11124bb52b8da3e47a"
+        private const val PUBLIC_KEY = "5ed58006f51feb029d1240da98b047c6"
         val SUBSCRIBER_SCHEDULER: Scheduler = Schedulers.io()
         val OBSERVER_SCHEDULER: Scheduler = AndroidSchedulers.mainThread()
 
         fun getDefaultQueryParams(): MutableMap<String, String> {
 
+            val timestamp = System.currentTimeMillis().toString()
+            val string = timestamp + PRIVATE_KEY + PUBLIC_KEY
+            val hash = MessageDigest.getInstance("MD5").digest(string.toByteArray(UTF_8)).joinToString("") { "%02x".format(it) }
+
             val queryParams: MutableMap<String, String> = HashMap()
-            queryParams["apikey"] = "5ed58006f51feb029d1240da98b047c6"
-            queryParams["ts"] = "1"
-            queryParams["hash"] = "050b4ab4753325c5f5b3538c50e5c4e7"
+            queryParams["apikey"] = PUBLIC_KEY
+            queryParams["ts"] = timestamp
+            queryParams["hash"] = hash
+
             return queryParams
         }
     }
