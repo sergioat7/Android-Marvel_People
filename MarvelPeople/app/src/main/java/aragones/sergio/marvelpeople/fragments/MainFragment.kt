a272591d4c732721e7a67babab95c0610087d7b6
Sergio Aragonés <sergioat7@gmail.com>
2020-11-03 00:41:09 +0100

    Created Main view

---
+++ MarvelPeople/app/src/main/java/aragones/sergio/marvelpeople/fragments/MainFragment.kt
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2020 Sergio Aragonés. All rights reserved.
+ * Created by Sergio Aragonés on 3/11/2020
+ */
+
+package aragones.sergio.marvelpeople.fragments
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import aragones.sergio.marvelpeople.R
+import aragones.sergio.marvelpeople.viewmodels.MainViewModel
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ MarvelPeople/app/src/main/java/aragones/sergio/marvelpeople/viewmodels/MainViewModel.kt
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2020 Sergio Aragonés. All rights reserved.
+ * Created by Sergio Aragonés on 3/11/2020
+ */
+
+package aragones.sergio.marvelpeople.viewmodels
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
